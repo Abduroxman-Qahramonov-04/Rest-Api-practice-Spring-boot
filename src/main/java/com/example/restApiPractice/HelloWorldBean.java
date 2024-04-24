df3ef8b1047579cc258cd9360fdd47671bcc8ca9
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-04-24 13:45:30 +0500

    path parameters

--- src/main/java/com/example/restApiPractice/HelloWorldBean.java
+++ src/main/java/com/example/restApiPractice/HelloWorldBean.java
@@ -1,6 +1,7 @@
 package com.example.restApiPractice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +10,9 @@ public class HelloWorldBean {
     public HelloObject getObject(){
         return new HelloObject("New bean");
     }
+    @GetMapping(path = "path-variable/{name}")
+    public HelloObject helloWorldPathVariable(@PathVariable String name){
+        return new HelloObject(name);
+    }
+
 }
