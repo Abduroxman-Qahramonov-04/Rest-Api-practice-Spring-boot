929e1123aa1244cdedb5ae192d8e12d895e0dff7
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-04-30 23:37:05 +0500

    my first post request, creating new users

--- src/main/java/com/example/restApiPractice/user/UserController.java
+++ src/main/java/com/example/restApiPractice/user/UserController.java
@@ -1,9 +1,10 @@
 package com.example.restApiPractice.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -15,5 +16,16 @@ public class UserController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return this.userDaoService.findAll();
+    }
+    @GetMapping("/users/{id}")
+    public User retrieveUserById(@PathVariable Integer id){
+        return userDaoService.findById(id);
+    }
+    @PostMapping("/users")
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User user1 = userDaoService.addUser(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user1.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
--- src/main/java/com/example/restApiPractice/user/UserDaoService.java
+++ src/main/java/com/example/restApiPractice/user/UserDaoService.java
@@ -5,17 +5,30 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Component
 public class UserDaoService {
-
+    private static Integer userCount = 0;
     private static List<User> users = new ArrayList<>();
     static {
-        users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(3,"Jim", LocalDate.now().minusYears(10)));
+        users.add(new User(++userCount,"Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount,"Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++userCount,"Jim", LocalDate.now().minusYears(10)));
     }
     public List<User> findAll(){
         return users;
+    }
+    public User findById(Integer id){
+        for(User user : users){
+            if(Objects.equals(user.getId(), id))
+                return user;
+        }
+        return null;
+    }
+    public User addUser(User user){
+        user.setId(++userCount);
+        users.add(user);
+        return user;
     }
 }
