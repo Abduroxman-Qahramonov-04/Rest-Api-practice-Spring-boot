3ac57e2bb4dd0b188d31957f9ebb312e13fa05d8
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-07-17 16:16:57 +0500

    added internationalization

--- src/main/java/com/example/restApiPractice/helloWorldPractice/HelloWorldBean.java
+++ src/main/java/com/example/restApiPractice/helloWorldPractice/HelloWorldBean.java
@@ -1,11 +1,19 @@
 package com.example.restApiPractice.helloWorldPractice;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldBean {
+    private MessageSource messageSource;
+    public HelloWorldBean(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping(path = "/get-bean")
     public HelloObject getObject(){
         return new HelloObject("New bean");
@@ -13,6 +21,11 @@ public class HelloWorldBean {
     @GetMapping(path = "path-variable/{name}")
     public HelloObject helloWorldPathVariable(@PathVariable String name){
         return new HelloObject(name);
+    }
+    @GetMapping(path = "/hello-in18")
+    public String helloWorldIn18(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default", locale);
     }
 
 }
