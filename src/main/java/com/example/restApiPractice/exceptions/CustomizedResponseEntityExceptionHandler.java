8a7433cf60bc2ada6dec330fd893a81c15d4c9ac
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-07-17 15:07:47 +0500

    added swagger ui - it is used to create documentation for the rest api

--- src/main/java/com/example/restApiPractice/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restApiPractice/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -1,17 +1,15 @@
 package com.example.restApiPractice.exceptions;
-import com.example.restApiPractice.user.User;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @ControllerAdvice
