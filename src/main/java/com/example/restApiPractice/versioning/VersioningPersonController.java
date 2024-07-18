19dc2d6d69416e20fbc4091f5110e5f03a784405
Abduroxman-Qahramonov-04 <abdurohman.kahramonov@servpoint.org>
2024-07-19 01:13:36 +0500

    media type and header and request parameter versioning

--- src/main/java/com/example/restApiPractice/versioning/VersioningPersonController.java
+++ src/main/java/com/example/restApiPractice/versioning/VersioningPersonController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+    //Url version
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
         return new PersonV1("Somebody");
@@ -15,4 +16,34 @@ public class VersioningPersonController {
 
         return new PersonV2("Somebody","surname");
     }
+    //Request parameter version
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Somebody");
+    }
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2("Somebody","surname");
+    }
+    //header versioning
+
+    @GetMapping(path = "/person", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonHeader(){
+        return new PersonV1("Somebody");
+    }
+    @GetMapping(path = "/person", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonHeader(){
+        return new PersonV2("Somebody","surname");
+    }
+    //Media type/Accept header versioning
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader(){
+        return new PersonV1("Somebody");
+    }
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader(){
+        return new PersonV2("Somebody","surname");
+    }
+
+
 }
